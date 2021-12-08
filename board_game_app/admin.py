d6b51bc7e0abe86583d1edc669a15795b9ddf808
Lassdi <lassi.lahti@edu.turkuamk.fi>
2021-12-08 18:48:58 +0200

    part 1

--- board_game_app/admin.py
+++ board_game_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Boardgame, Review
 
-# Register your models here.
+admin.site.register(Boardgame)
+admin.site.register(Review)
--- board_game_app/models.py
+++ board_game_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Boardgame(models.Model):
+
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class Review(models.Model):
+
+    boardgame = models.ForeignKey(Boardgame, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.text[:50]}..."
