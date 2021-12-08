3882b3c42836e758836d34f9cdbb96465d26cec6
rooperitola <ritola.matias@gmail.com>
2021-12-08 18:59:58 +0200

    part2
    Urls and views

--- board_game_app/views.py
+++ board_game_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Boardgame
+from .forms import Boardgame, BoardgameForm
 
-# Create your views here.
+
+def index(request):
+    #the home page for app
+    return render(request, 'Board_game_app/index.html')
+
+def new_Boardgame(request):
+    if request.method != 'POST':
+        form = BoardgameForm
+    else:
+        form = BoardgameForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Board_game_app:topics')
+        context = {'form':form}
+        return render(request, 'Board_game_app/new_Boardgame.html', context)
