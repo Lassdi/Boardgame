c897150b9b35f08a437dc8ca070064e49770b4e3
Lassdi <lassi.lahti@edu.turkuamk.fi>
2021-12-10 18:34:44 +0200

    part 11
    reviews and editing reviews

--- board_game_app/urls.py
+++ board_game_app/urls.py
@@ -5,9 +5,10 @@ from . import views
 app_name = 'board_game_app'
 
 urlpatterns = [
-    #Home page
-    path('', views.index, name='index'),
-    path('boardgame/', views.new_boardgame, name = 'new_boardgame'),
-    path('boardgames/', views.boardgames, name= 'boardgames'),
-    path('boardgames/<int:topic_id>/', views.boardgame, name= 'boardgame')
+    path('', views.index, name='index'), #Home page
+    path('boardgame/', views.new_boardgame, name = 'new_boardgame'), #Adding a new board game
+    path('boardgames/', views.boardgames, name= 'boardgames'), #List of all board games
+    path('boardgames/<int:boardgame_id>/', views.boardgame, name= 'boardgame'), #Individual boardgame
+    path("new_review/<int:boardgame_id>/", views.new_review, name="new_review"), #Writing a new review
+    path("edit_review/<int:review_id>/", views.edit_review, name="edit_review"), #Edoting a review
 ]
