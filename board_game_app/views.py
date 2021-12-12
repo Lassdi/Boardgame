f4e54699367c21e278c9e88705ca65a7b1bfdb5b
rooperitola <ritola.matias@gmail.com>
2021-12-12 11:29:06 +0200

    part12

--- board_game_app/views.py
+++ board_game_app/views.py
@@ -1,6 +1,8 @@
+from abc import abstractmethod
+from django.http import request
 from django.shortcuts import render, redirect
-from .models import Boardgame
-from .forms import Boardgame, BoardgameForm
+from .models import Boardgame, Review
+from .forms import Boardgame, BoardgameForm, ReviewForm
 
 
 def index(request):
@@ -18,8 +20,6 @@ def boardgame(request, boardgame_id):
     context = {"boardgame":boardgame, "reviews":reviews}
     return render(request,"board_game_app/boardgame.html",context)
 
-
-
 def new_boardgame(request):
     if request.method != "POST":
         form = BoardgameForm()
@@ -30,3 +30,32 @@ def new_boardgame(request):
             return redirect("board_game_app:boardgames")
     context = {"form":form}
     return render(request, "board_game_app/new_boardgame.html", context)
+
+def new_review(request, boardgame_id):
+    boardgame = Boardgame.objects.get(id = boardgame_id)
+
+    if request.method != 'POST':
+        form = ReviewForm()
+    else:
+        form = ReviewForm(data = request.POST)
+        if form.is.valid():
+            new_review = form.save(commit = False)
+            new_review.boardgame = boardgame
+            new_review.save()
+            return redirect('board_game_app:boardgame', boardgame_id = boardgame_id)    
+    context = {'boardgame': boardgame, 'form': form}
+    return render(request, 'board_game_app/new_review.html', context)
+
+def edit_review():
+    review = Review.objects.get(id = review_id)
+    boardgame = review.boardgame
+
+    if request.method != 'POST':
+        form = ReviewForm(instance = review)
+    else:
+        form = ReviewForm(instance = review, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('board_game_app:boardgame', boardgame_id = boardgame.id)
+    context = {'review': review, 'boardgame': boardgame, 'form': form}
+    return render(request, 'board_game_app/edit_review.html', context)
